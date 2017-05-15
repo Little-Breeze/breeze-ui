136d0230bbe371eda3b0d5d966a7f1b1c306581b
zhangn <ning.zhang@dianrong.com>
2017-05-15 18:26:03 +0800

    init

--- src/components/util.js
+++
@@ -1,9 +0,0 @@
-export const getScrollTop = () => {
-  let curTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
-  return curTop;
-};
-
-export const setScrollTop = val => {
-  document.body.scrollTop = val;
-  document.documentElement.scrollTop = val;
-}
