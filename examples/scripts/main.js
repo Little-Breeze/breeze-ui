7b82bf9dd69d6d2065d9ed718c0e3d469bc4252e
zhangn <ning.zhang@dianrong.com>
2017-05-15 18:25:36 +0800

    init

--- examples/scripts/main.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom';
-import App from './app';
-
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
-
-ReactDOM.render(<App />, document.getElementById('app'));
